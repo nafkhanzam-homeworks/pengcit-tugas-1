a7bd9eff93c44658ceddfa12a54ec26ee4a314e7
fadhilrafiii <fadhilrafi90@gmail.com>
2022-02-18 18:00:10 +0700

    Fix histogram specification algorithm

--- myhistspec.m
+++ myhistspec.m
@@ -1,55 +1,62 @@
-function histspec = myhistspec(inputImg, targetImg)
-    input = inputImg;
-    [rowI, columnI, rgbI] = size(input);
-    histInput = myhisteq(input);
-    sEq = histInput;
-    sArr = floor(sum_array(hist(sEq(:), 0:255) / (rowI * columnI * rgbI)) * 255);
-    
-    target = targetImg;
-    [rowT, columnT, rgbT] = size(target);
-    histTarget = myhisteq(target);
-    vEq = histTarget;
-    vArr = floor(sum_array(hist(vEq(:), 0:255) / (rowT * columnT * rgbT)) * 255);
+function result = myhistspec(input, target)
+    temp = input;
+    histInput = myHistogram(input);
+    InputEq = floor(sumArray(histInput) * 255);
 
-    invArr = zeros(1, 256);
+    histTarget = myHistogram(target);
+    TargetEq = floor(sumArray(histTarget) * 255);
+    
+    InvHist = zeros(1, 256);
     for i = 1:256
-        minVal = abs(sArr(i) - vArr(1));
         minIdx = 1;
+        minVal = abs(InputEq(i) - TargetEq(1));
+
         for j = 2:256
-            if (minVal > abs(sArr(i) - vArr(j)))
-                minVal = abs(sArr(i) - vArr(j));
+            diff = abs(InputEq(i) - TargetEq(j));
+            if (diff < minVal)
+                minVal = diff;
                 minIdx = j - 1;
             end
         end
-        invArr(i) = minIdx;
+
+        InvHist(i) = minIdx;
     end
 
-    for m = 1:rowI
-        for n = 1:columnI
-            for k = 1:rgbI
-                input(m, n, k) = invArr(input(m, n, k) + 1);
+    [row, col, rgb] = size(input);
+    for i = 1:row
+        for j = 1:col
+            for k = 1:rgb
+                temp(i, j, k) = InvHist(input(i, j, k));
             end
         end
     end
 
-    histspec = uint8(input);
+    result = temp;
 end
 
-function m = freq_map(im)
-    m = zeros(1, 256);
-    [row, col, rgb] = size(im);
-    for i=1:1:row
-        for j=1:1:col
-            for k=1:1:rgb
-                m(im(i, j, k) + 1) = m(im(i, j, k) + 1) + 1;
+function result = sumArray(arr)
+    temp = zeros(1, 256);
+    sum = 0;
+    for i = 1:256
+        sum = sum + arr(i);
+        temp(i) = sum;
+    end
+    
+    result = temp;
+end
+
+function histogram = myHistogram(img)
+    [row, col, rgb] = size(img);
+    
+    histogram = zeros(1, 256);
+    for i = 1:row
+        for j = 1:col
+            for k = 1:rgb
+                pixVal = img(i, j, k);
+                histogram(pixVal + 1) = histogram(pixVal + 1) + 1;
             end
         end
     end
-end
 
-function m = sum_array(arr)
-    m = arr;
-    for i=2:256
-        m(i) = m(i-1) + m(i);
-    end
+    histogram = histogram / (row * col * rgb);
 end
